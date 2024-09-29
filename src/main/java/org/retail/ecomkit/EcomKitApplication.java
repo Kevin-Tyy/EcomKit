8b94fa13c0d9aecbc81d417358914c57efce5bff
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-09-29 16:26:48 +0200

    new: initial commit

---
+++ src/main/java/org/retail/ecomkit/EcomKitApplication.java
@@ -0,0 +1,13 @@
+package org.retail.ecomkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomKitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/retail/ecomkit/EcomKitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.retail.ecomkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
